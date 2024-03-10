ea471c6bba1dd505dde35737e8f412fa44634146
Chinmoy Bora <chinmoyborah100@gmail.com>
2024-03-10 13:58:16 +0530

    New files added

--- src/components/task_2.js
+++ src/components/task_2.js
@@ -15,9 +15,13 @@ const Task2 = () => {
 
     return (
         <div className="task">
-            <h1>Task 2:</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <img src="/resource/download.jpeg" alt="task1_img"  />
+            <h1>Task 2 : Disposition Blend</h1>
+            {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> */}
+           <div className='task_2_img'> <img src="/resource/Compete.png" alt="task2_img"  />
+            <img src="/resource/Excel.png" alt="task2_img"  />
+            <img src="/resource/Code.png" alt="task2_img"  />
+            </div>
+            <h3>WARNING : Be careful while typing !!!</h3>
             <form onSubmit={handleSubmit} >
                 <label htmlFor="task2Input">Task 2 Answer:</label>
                 <input 
